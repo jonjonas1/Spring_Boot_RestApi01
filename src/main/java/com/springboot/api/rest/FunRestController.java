8a92ea6378fbb0a6f60e5fd03556a184e25ccc47
ojuraev@hotmail.com <ojuraev@hotmail.com>
2022-02-08 23:21:34 -0600

    first masters

---
+++ src/main/java/com/springboot/api/rest/FunRestController.java
@@ -0,0 +1,16 @@
+package com.springboot.api.rest;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+	
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello All Time is: " + LocalDateTime.now();
+	}
+	
+}
