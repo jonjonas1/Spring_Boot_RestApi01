8810a6942cb839a07fff296d64ecedff407fe42f
ojuraev@hotmail.com <ojuraev@hotmail.com>
2022-02-09 21:49:52 -0600

    app properties

--- src/main/java/com/springboot/api/rest/FunRestController.java
+++ src/main/java/com/springboot/api/rest/FunRestController.java
@@ -2,11 +2,18 @@ package com.springboot.api.rest;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+	
+	@Value("${coach.name}") // custom name from properties field
+	private String coachName;
+	
+	@Value("${team.name}")
+	private String teamName;
 	
 	@GetMapping("/")
 	public String sayHello() {
@@ -15,7 +22,7 @@ public class FunRestController {
 	
 	@GetMapping("/work")
 	public String workout() {
-		return "Doing very good exercises, Good Job";
+		return "Doing very good exercises, Good Job "+coachName+" "+ teamName;
 	}
 	
 }
